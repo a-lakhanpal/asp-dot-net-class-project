7e43aea43ba0c3e1a7c7c244de65f1aa0a2ec6dc
Ankur L <ankur.lakhan@hotmail.com>
2019-07-31 17:01:01 +1200

    Add files via upload

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        public int CourseId{get; set;}
+        public string Title { get; set; }
+        public string Credits { get; set; }
+
+        public Course()
+        {
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ContosoUniversity.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public string Grade {get; set;}
+            public Enrollment()
+        {
+        }
+    }
+}
